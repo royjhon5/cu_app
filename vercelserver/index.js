a612e91069f26394ff8e81a1402b6e2e179c98a3
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2024-05-23 21:52:00 +0800

    update: remove some codes

--- vercelserver/index.js
+++ vercelserver/index.js
@@ -3,9 +3,7 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser()); 
+require('./routes/routerManager')(app);
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
