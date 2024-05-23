4f42f3b7c0e07352c2e6cecaec278345ad6959b4
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2024-05-23 21:48:16 +0800

    update: serverless

--- vercelserver/index.js
+++ vercelserver/index.js
@@ -3,6 +3,23 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); 
+app.use(cors({
+    origin: (origin, callback) => {
+        const allowedOrigins = ["http://localhost:3000", "http://localhost:3100"];
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+    optionsSuccessStatus: 204
+}));
+
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
