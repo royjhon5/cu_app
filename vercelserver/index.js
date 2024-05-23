78efa4cd001aa07f830d0d4e2819d696016a3349
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2024-05-23 22:30:51 +0800

    update: added new cors origins

--- vercelserver/index.js
+++ vercelserver/index.js
@@ -9,6 +9,19 @@ const PORT = 8000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); 
+app.use(cors({
+  origin: (origin, callback) => {
+      const allowedOrigins = ["http://localhost:3000", "http://localhost:3100"];
+      if (allowedOrigins.includes(origin) || !origin) {
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  credentials: true,
+  optionsSuccessStatus: 204
+}));
 require('./routes/routerManager')(app);
 app.get('/', (req, res) => {
   res.send('This is my serverless server')
