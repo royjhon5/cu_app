942295c4fa146707ae5a98e85c15880edcee0c1b
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2024-05-23 22:29:42 +0800

    update: added cookie parser

--- vercelserver/index.js
+++ vercelserver/index.js
@@ -1,12 +1,14 @@
 
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8000
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); 
 require('./routes/routerManager')(app);
 app.get('/', (req, res) => {
   res.send('This is my serverless server')
