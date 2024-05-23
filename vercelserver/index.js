d93918a5e547c71c8d0a6c27e33bcaf21d0c878d
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2024-05-23 22:23:05 +0800

    update: testng 3 line of codes

--- vercelserver/index.js
+++ vercelserver/index.js
@@ -1,8 +1,13 @@
 
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 8000
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); 
 require('./routes/routerManager')(app);
 app.get('/', (req, res) => {
   res.send('This is my serverless server')
