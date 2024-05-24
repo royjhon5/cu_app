9a4c8983d7633eb35024a17aaa59697dbf35cbe6
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2024-05-24 19:25:45 +0800

    update: removed mainroutes

--- admin/src/routes/index.jsx
+++ admin/src/routes/index.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import MainRoutes from "./Mainroutes.jsx";
 import AuthenticationRoutes from "./AuthenticationRoutes.jsx";
-const routes = createBrowserRouter([AuthenticationRoutes, MainRoutes])
+const routes = createBrowserRouter([AuthenticationRoutes])
 export default routes;
