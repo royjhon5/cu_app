4970a59df552331bfaaefa67cec57fe9466a579d
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2024-05-24 10:24:57 +0800

    update: package.json

--- client/src/api/http.jsx
+++ client/src/api/http.jsx
@@ -1,7 +1,8 @@
 import Axios from 'axios';
 
+const baseUrl = window.location.origin;
 const http = Axios.create({
-    baseURL: "https://cu-app-sample-server.vercel.app/api",
+    baseURL: baseUrl.split(':')[0]+':'+baseUrl.split(':')[1]+":8000/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
--- vercelserver/routes/routerManager.js
+++
@@ -1,4 +0,0 @@
-var route_api = require('./api');
-module.exports = function(app){
-    app.use('/api', route_api);
-}
