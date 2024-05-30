63d866f4b5fde9e071d09ce5e2b2f4385c8e0f37
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2024-05-30 15:43:33 +0800

    update: added socket server

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -12,8 +12,7 @@ import ru from "javascript-time-ago/locale/ru.json";
 TimeAgo.addDefaultLocale(en)
 TimeAgo.addLocale(ru)
 import socketIO from 'socket.io-client';
-const baseUrl = window.location.origin;
-const socketConnect = socketIO.connect(baseUrl.split(':')[0]+':'+baseUrl.split(':')[1]+':8000');
+const socketConnect = socketIO.connect('https://cu-app-websocket-eec58bf9cc60.herokuapp.com');
 
 export const WebSocket = () => {
     return socketConnect;
