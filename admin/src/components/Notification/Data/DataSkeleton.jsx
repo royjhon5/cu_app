564b0dee669f12f606fa2d7e28dab0b937627afe
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2024-06-04 15:59:11 +0800

    update: test skeleton

---
+++ admin/src/components/Notification/Data/DataSkeleton.jsx
@@ -0,0 +1,8 @@
+
+const DataSkeleton = () => {
+  return (
+    <div>DataSkeleton</div>
+  )
+}
+
+export default DataSkeleton
--- admin/src/components/Notification/Data/index.jsx
+++ admin/src/components/Notification/Data/index.jsx
@@ -51,56 +51,85 @@ const NotificationData = () => {
 
   return (
     <>
-    {unRead.map((notificationData, index) => (
-      <ListContainer key={index}>
-      <NotifButton>
-        {notificationData.notif_status === 1 ? '' : <Identification />}
-          {loading? <SkeletonAvatar /> : <ListAvatar />}
-          <DataContainer>
-              <ListItemText sx={{
-                flex: '1 1 auto',
-                minWidth: 0,
-                margin: 0
-              }}>
-                <Box sx={{ marginBottom: '4px'}}>
-                  {loading ? <Skeleton variant="text" sx={{ fontSize: '15px' }} /> : ''}
-                    <Typography fontSize={'15px'}>{notificationData.first_name} {notificationData.last_name} - {notificationData.id_number}</Typography>
-                </Box>
+    {loading ? 
+    ( 
+      <>
+        {[...Array(5)].map((_, index) => (
+          <ListContainer key={index}>
+            <NotifButton>
+                <SkeletonAvatar />
+                <DataContainer>
+                  <ListItemText sx={{
+                  flex: '1 1 auto',
+                  minWidth: 0,
+                  margin: 0
+                }}>
+                    <Box sx={{ marginBottom: '4px'}}>
+                      <Skeleton variant="text" animation="wave" width={200} sx={{fontSize: '15px', width: '100%'}} />
+                    </Box>
+                    <Stack sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    lineHeight: 1.5,
+                    fontSize: '0.75rem',
+                    fontWeight: '400'
+                  }}>
+                    <Skeleton variant="text" animation="wave" width={150} sx={{fontSize: '15px'}} />
+                    </Stack>
+                  </ListItemText>
+                </DataContainer>
+            </NotifButton>
+          </ListContainer>
+        ))}
+      </>
+    ) : ( 
+      unRead.map((notificationData, index) => (
+        <ListContainer key={index}>
+        <NotifButton>
+          {notificationData.notif_status === 1 ? '' : <Identification />}
+             <ListAvatar />
+            <DataContainer>
+                <ListItemText sx={{
+                  flex: '1 1 auto',
+                  minWidth: 0,
+                  margin: 0
+                }}>
+                  <Box sx={{ marginBottom: '4px'}}>
+                      <Typography fontSize={'15px'}>{notificationData.first_name} {notificationData.last_name} - {notificationData.id_number}</Typography>
+                  </Box>
+                  <Stack sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    lineHeight: 1.5,
+                    fontSize: '0.75rem',
+                    fontWeight: '400'
+                  }}>
+                      <ReactTimeAgo date={new Date(notificationData.time_created).getTime()} /> 
+                      <Box sx={{ width: '2px', height: '2px', backgroundColor: 'currentcolor', marginLeft: '4px', marginRight: '4px', borderRadius: '50%' }}></Box>
+                      {notificationData.type_of_notification}
+                  </Stack>
+                </ListItemText>
                 <Stack sx={{
                   display: 'flex',
                   flexDirection: 'row',
-                  alignItems: 'center',
-                  lineHeight: 1.5,
-                  fontSize: '0.75rem',
-                  fontWeight: '400'
+                  gap: '8px',
+                  marginTop: '12px'
                 }}>
-                    {loading ? <Skeleton variant="text" sx={{ fontSize: '0.75rem', width: '100%' }} /> : 
+                    {notificationData.notif_status === 1 ? '' 
+                    :
                     <>
-                    <ReactTimeAgo date={new Date(notificationData.time_created).getTime()} /> 
-                    <Box sx={{ width: '2px', height: '2px', backgroundColor: 'currentcolor', marginLeft: '4px', marginRight: '4px', borderRadius: '50%' }}></Box>
-                    {notificationData.type_of_notification}
+                    <Button variant="contained" color="primary" size="small" onClick={() => activateUser(notificationData.id_number)}>Accept</Button>
+                    <Button variant="contained" color="error" size="small">Decline</Button>
                     </>
                     }
                 </Stack>
-              </ListItemText>
-              <Stack sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                gap: '8px',
-                marginTop: '12px'
-              }}>
-                  {notificationData.notif_status === 1 ? '' 
-                  :
-                  <>
-                  <Button variant="contained" color="primary" size="small" onClick={() => activateUser(notificationData.id_number)}>Accept</Button>
-                  <Button variant="contained" color="error" size="small">Decline</Button>
-                  </>
-                  }
-              </Stack>
-          </DataContainer>
-      </NotifButton>
-  </ListContainer>
-    ))}
+            </DataContainer>
+          </NotifButton>
+        </ListContainer>
+      ))
+    )}
     </>
   )
 }
