35f32b7f633dcb459a1af2fadc9ee84d767b0ca9
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2024-06-04 16:50:39 +0800

    update: final width

--- admin/src/components/Notification/Data/index.jsx
+++ admin/src/components/Notification/Data/index.jsx
@@ -93,7 +93,7 @@ const NotificationData = () => {
         gap: 1.5,
         height: '85vh'
       }}>
-        <img src={NoNotificationIcon} style={{ height: '5%', width: '15%'}} />
+        <img src={NoNotificationIcon} style={{ height: '5%', width: '10%'}} />
         <Typography variant="body2" align="center">
           No notification
         </Typography>
